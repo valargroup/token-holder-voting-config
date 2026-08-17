02eba49ca2640daee22875e061bf578024d11a31
Adam Tucker <adam@osmosis.team>
2026-08-17 15:08:25 -0600

    Document portable temp directory handling

--- scripts/tests/lib/test-helpers.sh
+++ scripts/tests/lib/test-helpers.sh
@@ -3,5 +3,6 @@
 make_test_temp_dir() {
   local name="$1"
   local temp_root="${TMPDIR:-/tmp}"
+  # An explicit X template works with both BSD and GNU mktemp.
   mktemp -d "${temp_root%/}/${name}.XXXXXXXXXX"
 }
